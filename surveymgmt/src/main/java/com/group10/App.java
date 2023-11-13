44ea48ca51eb437f343d1d92de4615510c7229c1
ca-resse <caressetzn@gmail.com>
2023-11-13 20:24:38 +0800

    13 Nov 2023

--- surveymgmt/src/main/java/com/group10/App.java
+++ surveymgmt/src/main/java/com/group10/App.java
@@ -18,19 +18,6 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        // Collection<SurveyData> list = Files.readAllLines(new File("surveylist.txt").toPath()).stream().map(line -> {
-        //     String[] details = line.split("\t");
-        //     SurveyData sd = new SurveyData();
-        //     sd.setSurveyID(details[0]);
-        //     sd.setSurveyTitle(details[1]);
-        //     sd.setSurveyDetails(details[2]);
-        //     sd.setCreatorName(details[3]);
-        //     return sd;
-        // })
-        // .collect(Collectors.toList());
-
-        // ObservableList<SurveyData> details =FXCollections.observableArrayList(list);
-
         scene = new Scene(loadFXML("surveylist"), 640, 480);
         stage.setScene(scene);
         stage.setTitle("Taylor's Survey Management System");
