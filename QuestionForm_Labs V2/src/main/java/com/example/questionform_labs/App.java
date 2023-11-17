3a20b7533fa5c1df216c0789d6634211d6f96f76
DeltaV <70023762+DeltaV-D5@users.noreply.github.com>
2023-11-17 09:29:18 +0800

    Add files via upload

---
+++ QuestionForm_Labs V2/src/main/java/com/example/questionform_labs/App.java
@@ -0,0 +1,60 @@
+package com.example.questionform_labs;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+//        try (Reader reader = new FileReader("src/main/resources/JSON/Surveys/Survey_0001.json")) {
+//            JSONObject data = (JSONObject) new JSONParser().parse(reader);
+//            System.out.println(data.get("title"));
+//        } catch (IOException | ParseException e) {
+//            e.printStackTrace();
+//        }
+
+
+        // Standard-ass loading
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/FormSurvey.fxml"));
+        Scene scene = new Scene(root, 1000, 600);
+        String str = "0001";
+        int num = 420;
+
+//        System.out.println(Integer.parseInt(str));
+        System.out.println(String.format("%04d", num));
+
+        // Load CSS
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+        // Press "Esc" to close
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.ESCAPE) {
+                Platform.exit();
+            }
+        });
+
+        stage.setTitle("QuestionForm Labs");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
