30190caf308bced4a92c539f31fbde89e093f80f
ca-resse <caressetzn@gmail.com>
2023-11-19 17:08:24 +0800

    Added a student view for surveylist

--- surveymgmt/src/main/java/com/group10/StudentController.java
+++ surveymgmt/src/main/java/com/group10/StudentController.java
@@ -17,10 +17,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
-public class SurveyListController {
-
-    @FXML
-    private Button createSurvey_btn;
+public class StudentController {
 
     @FXML
     private TableColumn<Survey, String> creatorname_col;
@@ -29,13 +26,16 @@ public class SurveyListController {
     private Button logout_btn;
 
     @FXML
-    private Button search_btn;
-
-    @FXML
     private Button refresh_btn;
 
     @FXML
+    private Button search_btn;
+
+    @FXML
     private TextField search_field;
+
+    @FXML
+    private TableColumn<Survey, String> surveydetails_col;
 
     @FXML
     private TableColumn<Survey, Integer> surveyid_col;
@@ -50,18 +50,10 @@ public class SurveyListController {
     private TableColumn<Survey, String> surveytitle_col;
 
     @FXML
-    private TableColumn<Survey, String> surveydetails_col;
-    
-    @FXML
-    private void switchToSurveyForm(ActionEvent e) throws IOException {
-        App.setRoot("surveyform");
-    }
-
-    @FXML
-    public void onClick_logout_btn (ActionEvent e) throws IOException{
+    void onClick_logout_btn(ActionEvent event) throws IOException {
         App.setRoot("primary");
     }
-    
+
     public void initialize(){
         //sets the reference value for each respective column
         surveyid_col.setCellValueFactory(new PropertyValueFactory<>("surveyID")); //must match surveyID variable from the Survey class 
@@ -77,20 +69,23 @@ public class SurveyListController {
             // Loop through the array and add data to Tableview
             for (Object obj : array){
                 JSONObject surveyObject = (JSONObject) obj;
-                String surveyid = (String) surveyObject.get("survey_id");
-                String surveytitle = (String) surveyObject.get("survey_title");
-                String creatorname = (String) surveyObject.get("creator_name");
-                String surveydetails = (String) surveyObject.get("survey_details");
-                //Create new survey object
-                Survey survey = new Survey(Integer.parseInt(surveyid), surveytitle, creatorname, surveydetails);
-            
-                // populate tableview with data from the survey object
-                surveylist_table.getItems().add(survey);
+
+                // Students should only be able to access surveys that are "started" and "not blocked".
+                // Only surveys with isStarted set to true and isBlocked set to false will be shown in the students' tableview
+                if ((boolean) surveyObject.get("isStarted") == true && (boolean) surveyObject.get("isBlocked") == false){
+                    String surveyid = (String) surveyObject.get("survey_id");
+                    String surveytitle = (String) surveyObject.get("survey_title");
+                    String creatorname = (String) surveyObject.get("creator_name");
+                    String surveydetails = (String) surveyObject.get("survey_details");
+                    //Create new survey object
+                    Survey survey = new Survey(Integer.parseInt(surveyid), surveytitle, creatorname, surveydetails);
                 
+                    // populate tableview with data from the survey object
+                    surveylist_table.getItems().add(survey);
+                }   
             }
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
     }
-    
 }
