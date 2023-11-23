928122f143963c583123eefec0dbced5678d0e10
ca-resse <caressetzn@gmail.com>
2023-11-23 17:41:48 +0800

    please work

--- surveymgmt V2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.group10 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires json.simple;
-    requires org.json;
-
-    opens com.group10 to javafx.fxml;
-    exports com.group10;
-}
